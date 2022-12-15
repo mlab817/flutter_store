fc3e40a93451c10e852cfa6413d99552a31869a4
Mark Lester Bolotaolo <mlab817@gmail.com>
2022-12-15 18:24:39 +0800

    created an animated download button

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,256 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+// import 'package:flutter/rendering.dart';
 
 void main() {
+  // debugPaintSizeEnabled = true;
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  final DownloadStatus downloadStatus = DownloadStatus.fetchingDownload;
+  final double progress = 0.0;
+
+  void startDownload() {}
+  void stopDownload() {}
+  void openDownload() {}
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.amber,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('My Store'),
+            elevation: 0.0,
+          ),
+          body: Column(
+            children: [
+              SizedBox(
+                width: 100.0,
+                child: DownloadButton(
+                  status: DownloadStatus.notDownloaded,
+                  downloadProgress: progress,
+                  onDownload: startDownload,
+                  onCancel: stopDownload,
+                  onOpen: openDownload,
+                ),
+              ),
+              DownloadButton(
+                status: DownloadStatus.fetchingDownload,
+                downloadProgress: progress,
+                onDownload: startDownload,
+                onCancel: stopDownload,
+                onOpen: openDownload,
+              ),
+              DownloadButton(
+                status: DownloadStatus.downloading,
+                downloadProgress: progress,
+                onDownload: startDownload,
+                onCancel: stopDownload,
+                onOpen: openDownload,
+              ),
+              SizedBox(
+                width: 100.0,
+                child: DownloadButton(
+                  status: DownloadStatus.downloaded,
+                  downloadProgress: progress,
+                  onDownload: startDownload,
+                  onCancel: stopDownload,
+                  onOpen: openDownload,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+enum DownloadStatus {
+  notDownloaded,
+  fetchingDownload,
+  downloading,
+  downloaded,
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+@immutable
+class DownloadButton extends StatelessWidget {
+  const DownloadButton({
+    super.key,
+    required this.status,
+    this.transitionDuration = const Duration(
+      milliseconds: 500,
+    ),
+    required this.onDownload,
+    required this.onCancel,
+    required this.onOpen,
+    this.downloadProgress = 0.0,
+  });
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  // arguments passed from the parent
+  final DownloadStatus status;
+  final Duration transitionDuration;
+  final double downloadProgress;
+
+  final VoidCallback onDownload;
+  final VoidCallback onCancel;
+  final VoidCallback onOpen;
+
+  bool get _isDownloading => status == DownloadStatus.downloading;
+
+  bool get _isFetching => status == DownloadStatus.fetchingDownload;
+
+  bool get _isDownloaded => status == DownloadStatus.downloaded;
+
+  void _onPressed() {
+    switch (status) {
+      case DownloadStatus.notDownloaded:
+        onDownload();
+        break;
+      case DownloadStatus.fetchingDownload:
+        // do nothing.
+        break;
+      case DownloadStatus.downloading:
+        onCancel();
+        break;
+      case DownloadStatus.downloaded:
+        onOpen();
+        break;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return GestureDetector(
+      onTap: _onPressed,
+      child: Stack(
+        children: [
+          ButtonShapeWidget(
+            transitionDuration: transitionDuration,
+            isDownloaded: _isDownloaded,
+            isDownloading: _isDownloading,
+            isFetching: _isFetching,
+          ),
+          Positioned.fill(
+            child: AnimatedOpacity(
+              duration: transitionDuration,
+              opacity: _isDownloading || _isFetching ? 1.0 : 0.0,
+              curve: Curves.ease,
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  ProgressIndicatorWidget(
+                    downloadProgress: downloadProgress,
+                    isDownloading: _isDownloading,
+                    isFetching: _isFetching,
+                  ),
+                  if (_isDownloading)
+                    const Icon(
+                      Icons.stop,
+                      size: 14.0,
+                      color: CupertinoColors.activeBlue,
+                    ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    );
+  }
+}
+
+@immutable
+class ButtonShapeWidget extends StatelessWidget {
+  const ButtonShapeWidget(
+      {super.key,
+      required this.transitionDuration,
+      required this.isDownloaded,
+      required this.isDownloading,
+      required this.isFetching});
+
+  final Duration transitionDuration;
+  final bool isDownloaded;
+  final bool isDownloading;
+  final bool isFetching;
+
+  @override
+  Widget build(BuildContext context) {
+    // define shapes
+    var shape = const ShapeDecoration(
+        shape: StadiumBorder(), color: CupertinoColors.lightBackgroundGray);
+
+    // if downloading, overwrite shape
+    if (isDownloading || isFetching) {
+      shape = ShapeDecoration(
+          shape: const CircleBorder(), color: Colors.white.withOpacity(0.0));
+    }
+
+    return AnimatedContainer(
+      duration: transitionDuration,
+      curve: Curves.ease,
+      width: double.infinity,
+      decoration: shape,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 6.0),
+        child: AnimatedOpacity(
+          opacity: isDownloading || isFetching ? 0.0 : 1.0,
+          curve: Curves.ease,
+          duration: transitionDuration,
+          child: Text(
+            isDownloaded ? 'OPEN' : 'GET',
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.button?.copyWith(
+                fontWeight: FontWeight.bold, color: CupertinoColors.activeBlue),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+@immutable
+class ProgressIndicatorWidget extends StatelessWidget {
+  const ProgressIndicatorWidget({
+    super.key,
+    required this.downloadProgress,
+    required this.isDownloading,
+    required this.isFetching,
+  });
+
+  final double downloadProgress;
+  final bool isDownloading;
+  final bool isFetching;
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1,
+      child: TweenAnimationBuilder<double>(
+        tween: Tween(begin: 0, end: downloadProgress),
+        duration: const Duration(milliseconds: 200),
+        builder: (context, progress, child) {
+          return CircularProgressIndicator(
+            backgroundColor: isDownloading
+                ? CupertinoColors.lightBackgroundGray
+                : Colors.white.withOpacity(0),
+            valueColor: AlwaysStoppedAnimation(isFetching
+                ? CupertinoColors.lightBackgroundGray
+                : CupertinoColors.activeBlue),
+            strokeWidth: 2,
+            value: isFetching ? null : progress,
+          );
+        },
+      ),
     );
   }
 }
